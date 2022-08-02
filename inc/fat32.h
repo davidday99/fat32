df11fb98b8cec9b7471d3b3bcd2f56e990fa9d78
David Day <david.day2017@gmail.com>
2022-08-01 21:29:39 -0500

    declare more functions for interface

--- inc/fat32.h
+++ inc/fat32.h
@@ -1,12 +1,17 @@
 #ifndef _FAT32_H_
 #define _FAT32_H_
 
+#include <stdint.h>
 #include "eeprom.h"
 #include "fat32_file.h"
+#include "fat32_dir.h"
 
 void format_fat32(struct EEPROM *mem);
 void init_fat32_fs(struct EEPROM *mem);
 uint8_t fat32_fs_valid(void);
 FAT32_FILE *fat32_open(char *path, FAT32_FILE *fptr);
+void fat32_close(FAT32_FILE *fptr);
+uint32_t fat32_read(FAT32_FILE *fptr);
+uint32_t fat32_write(FAT32_FILE *fptr);
 
 #endif /* _FAT32_H_ */
