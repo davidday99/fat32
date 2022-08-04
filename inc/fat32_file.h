3fd9ae7041227a902587f15c7023748a8cb00b8d
David Day <david.day2017@gmail.com>
2022-08-03 23:03:38 -0500

    add necessary param

--- inc/fat32_file.h
+++ inc/fat32_file.h
@@ -1,15 +1,22 @@
 #ifndef _FAT32_FILE_
 #define _FAT32_FILE_
 
-#define FAT32_MAX_FILENAME_LEN 12
+#include "fat32_internal.h"
+
+#define FAT32_MAX_FILENAME_LEN 11
 
 typedef struct _fat32_file {
-    char name[FAT32_MAX_FILENAME_LEN];
+    char name[FAT32_MAX_FILENAME_LEN + 1];
     uint32_t offset;
+    uint16_t clus_offset;
     uint32_t size;
+    uint32_t entry_addr;
+    uint32_t clus_base;
+    uint32_t clus_curr;
+    uint32_t is_dir;
 } FAT32_FILE;
 
-FAT32_FILE *_fat32_open(char *path, FAT32_FILE *fptr);
+FAT32_FILE *_fat32_open(FAT32_FS *fs, char *path, FAT32_FILE *fptr);
 
 #endif /* _FAT32_FILE */
 
